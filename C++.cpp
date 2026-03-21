a7244012045209948baba56c56db21793ccb7051
Qianying-Chuugu <2270538467@qq.com>
2026-03-21 16:57:17 +0800

    new file:   C++.cpp

---
+++ C++.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    const long long a = 5;
+    cout << "Hello World!" <<sizeof(a)<<endl;
+    cout << "Hello World!" <<sizeof(a);
+    system("pause");
+    return 0;
+}
