cde045daa5ba6d1b6a140d1c91b7f44c31b9312a
Qianying-Chuugu <2270538467@qq.com>
2026-04-01 20:41:43 +0800

    4.1

--- C++.cpp
+++ C++.cpp
@@ -2,15 +2,17 @@
 # include <iostream>
 using namespace std;
 
+int add(int b=10){
+    return b;
+}
+
+int add(int a)
+{
+    return a;
+}
+
 int main()
 {
-    int *p= new int[10];
-    for(int i=0;i<10;i++)
-    {
-        p[i]=i;
-    }
-    for(int i=0;i<10;i++)
-    {
-        cout<<p[i]<<" ";
-    }
+    int a=1,b=2;
+    cout << add(a) << endl;
 }
