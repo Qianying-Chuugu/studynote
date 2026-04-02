b25cc7dc109cff22b1ab95644c29e67dca4c1ee7
Qianying-Chuugu <2270538467@qq.com>
2026-04-02 20:20:49 +0800

    4.2

--- C++.cpp
+++ C++.cpp
@@ -1,26 +1,14 @@
-# include <string>
-# include <iostream>
-# include <math.h>
+#include "C++.h"
 using namespace std;
 
-class cirlce{
-    public:
-        int x;
-        int y;
-        int banjing;
-};
-
-class point{
-    public:
-        int x;
-        int y;
-    public:
-        void juli(int cir,int poi){
-            double ju = pow(c-poi.x)
-        }
-}
-
-int main()
-{
-
+int main(){
+    point a;
+    a.x=2;
+    a.y=9;
+    circle b;
+    b.x=4;
+    b.y=8;
+    b.bianchang=6.4;
+    b.bijiao(a);
+    system("pause");
 }
