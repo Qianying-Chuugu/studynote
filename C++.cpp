2537e29344d5acfdba052f1975ef7453106344b7
Qianying-Chuugu <2270538467@qq.com>
2026-04-07 15:31:38 +0800

    4.7

--- C++.cpp
+++ C++.cpp
@@ -2,39 +2,3 @@
 #include <string.h>
 using namespace std;
 
-class tree;
-
-class grass{
-    
-    public:
-    int a;
-    public:
-    void grassmade();
-};
-
-class tree{
-
-    friend void grass::grassmade();
-    private:
-    int m;
-    int n;
-    private:
-    void treemade(){
-        cout<<"hello,tree"<<endl;
-    }
-    tree():m(10),n(20){}
-
-};
-
-void grass::grassmade(){
-    tree A;
-    cout<<A.m<<" "<<A.n<<endl;
-    A.treemade();
-
-}
-
-
-int main(){
-   grass B;
-   B.grassmade();
-}
